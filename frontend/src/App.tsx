551dc593b2e329f68114b6a8e66b5e04f2448dd3
LeoKle <leon.kleinschmidt@outlook.de>
2023-09-30 11:19:25 +0200

    set-up frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+
+function App() {
+  return (
+    <>
+    </>
+  );
+}
+
+export default App;
