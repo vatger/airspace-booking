ef9fbc43e849e7dd828004efdd6dfe368429ab00
LeoKle <leon.kleinschmidt@outlook.de>
2023-10-31 18:42:14 +0100

    fix build error

--- src/backend/controllers/euup.controller.ts
+++ src/backend/controllers/euup.controller.ts
@@ -1,14 +1,15 @@
-import { NextFunction, Request, Response } from "express";
-import { EuupDocument } from "../models/euup.model";
-import euupService from "../services/euup.service";
+import { NextFunction, Request, Response } from 'express';
+
+import { EuupDocument } from '../models/euup.model';
+import euupService from '../services/euup.service';
 
 export async function getEuupData(
   req: Request,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) {
   try {
-    const areas: EuupDocument | null = await euupService.getEuupData();
+    const areas: EuupDocument | null | undefined = await euupService.getEuupData();
     res.json(areas);
   } catch (error) {
     console.error(error);
